7c1088ac96368fbd57afd2b3d60b22ed849f111c
Nayte91 <nayte91@gmail.com>
2021-09-30 15:32:51 +0200

    etape 0

--- Soluces/soluces.js
+++ Soluces/soluces.js
@@ -1,41 +1,37 @@
 /* fonction qui crée un élément HTML option, en fonction du texte envoyé.
 peu ou prou : "<option value="text">text</option>"
 On pourra ensuite l'insérer dans le sélecteur pour faire un joli menu déroulant. */
-const createOption = (text) => {
-    let element = document.createElement("option");
-    element.text = element.value = text;
+const createOptionTag = (text) => {
+    let element = document.createElement("option")
+    element.text = element.value = text
 
-    return element;
+    return element
 }
 
-const populateSelector = (selectorId, dataArray, index) => {
-    let selector = document.getElementById(selectorId);
+const populateSelector = (selector, dataArray, index, text) => {
     selector.innerHTML = "";
 
     let defaultOption = document.createElement("option");
-    defaultOption.text = "-- Please choose a " + selectorId + " --";
-    selector.add(defaultOption, null);
+    defaultOption.text = "-- Please choose a " + text + " --";
+    selector.add(defaultOption, null)
     
-    dataArray.forEach(row => {
-        /* row est un object, mais les propriétés d'un object peuvent être appelées
-        avec . ou avec []. Ici, [index] permet d'appeler dynamiquement,
-        en fonction d'un index qui change. 
-        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Property_Accessors#bracket_notation */
-        selector.add(createOption(row[index]), null);
-    });
+    /* row est un object, mais les propriétés d'un object peuvent être appelées
+    avec . ou avec [] ! Ici, [index] permet d'appeler dynamiquement,
+    en fonction d'un index qui change. 
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Property_Accessors#bracket_notation */
+    dataArray.forEach((row) => selector.add(createOptionTag(row[index]), null))
 }
 
-const populateMasterSelector = (selectorId, dataArray) => {
-    populateSelector(selectorId, dataArray, 'firstName');
+const populateMasterSelector = (selector, dataArray) => {
+    populateSelector(selector, dataArray, 'firstName', 'master')
 }
 
-const populateDogSelector = (selectorId, dataArray) => {
-    populateSelector(selectorId, dataArray, 'name');
+const populateDogSelector = (selector, dataArray) => {
+    populateSelector(selector, dataArray, 'name', 'dog')
 }
 
-const populateTableBody = (tableBodyId, dataObject) => {
-    let tableBody = document.getElementById(tableBodyId);
-    tableBody.innerHTML = "";
+const populateTableBody = (tableBody, dataObject) => {
+    tableBody.innerHTML = ""
 
     for (const prop in dataObject) {
         /* le if teste si la propriété que l'on va lire n'est pas un tableau ou un objet.
@@ -48,7 +44,31 @@ const populateTableBody = (tableBodyId, dataObject) => {
     }
 }
 
-populateMasterSelector('master', doggletData);
-populateTableBody('master-table-body', doggletData[0]);
-populateDogSelector('dog', doggletData[0].dogs)
-populateTableBody('dog-table-body', doggletData[0].dogs[0])
+/*  l'objet window en JavaScript représente la fenêtre (ou l'onglet) du navigateur.
+    un event est un moment où il se passe quelque chose (sans blague !).
+    un eventListener est un classique en programmation, cela permet d'éxecuter du code
+    quand il se passe un évènement donné.
+    Ici, on ajoute donc un EventListener à la fenêtre :
+        Quand la fenêtre est totalement chargée (le html, le javascript, ...) avec 'load',
+        On va lancer la callable suivante,
+            qui n'a pas de paramètre (d'où le ()), 
+            dans laquelle on lance 3 fonctions,
+            elle ne renvoit rien.
+    https://developer.mozilla.org/en-US/docs/Web/API/Window/load_event */
+window.addEventListener(
+    'load', 
+    () => {
+        populateMasterSelector(masterSelector, doggletData)
+        populateDogSelector(dogSelector, doggletData[0].dogs)
+        populateTableBody(dogTable, doggletData[0].dogs[0])
+    }
+)
+
+masterSelector.addEventListener(
+    'change', 
+    (e) => {
+        var selectedMasterData = doggletData.filter((master) => master.firstName == e.target.value)[0]
+        populateTableBody(masterTable, selectedMasterData)
+        populateDogSelector(dogSelector, selectedMasterData.dogs)
+    }
+)
---
+++ assets/tools.js
@@ -0,0 +1,8 @@
+// Variables qui représentent le menu déroulant
+const masterSelector = document.getElementById('master')
+const dogSelector = document.getElementById('dog')
+
+// Variables qui représentent le tableau et son corps
+const masterTable = document.getElementById('master-table')
+const masterTableBody = document.getElementById('master-table-body')
+const dogTable = document.getElementById('dog-table-body')
