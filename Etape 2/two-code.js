8414535ea51a13297b4e0a89d2246ed748e1864c
Nayte <nayte91@gmail.com>
2021-10-07 19:40:52 +0200

    Etape 2 OK

--- Etape 2/two-code.js
+++ Etape 2/two-code.js
@@ -1,11 +1,10 @@
 /* 
-    Cette fonction permet d'ajouter une entrée dans le menu déroulant.
-    En entrée, tu lui donnes le texte que tu veux afficher dans cette entrée.
-    Tu devras appeler cette fonction, elle te mâche le travail du coté du html, que tu n'as pas étudié.
-    Mais tu n'y touches pas ! Sauf si tu veux l'améliorer :p
+    Avant, cette variable était dans la fonction addOptionTag. Je l'ai sortie pour l'utiliser plus globalement, notamment tout en bas avec .addEventListener. 
 */
+const masterSelector = document.getElementById('master')
+const masterTableBody = document.getElementById('master-table').getElementsByTagName('tbody')[0]
+
 const addOptionTag = (text) => {
-    let masterSelector = document.getElementById('master')
     let element = document.createElement("option")
     element.text = element.value = text
 
@@ -13,20 +12,50 @@ const addOptionTag = (text) => {
 }
 
 /* 
-    Ton code doit être dans la fonction ci-dessous !
-    On sait qu'elle sera executée lors du chargement de la page, qu'elle n'a aucun paramètre, et qu'elle ne renvoit rien.
-    Les datas sont dans la variable doggletData, que tu peux aller lire dans assets/data.js. Une structure que tu dois savoir lire !
-    Ton but va être d'écrire un bout de code qui va ajouter un élément (option tag) dans le menu select, un pour chaque maître.
+    Fonction pour remplir le menu déroulant des maîtres dynamiquement.
+    Se lance au chargement de la page.
+    Etudiée à l'étape 1. J'y ai rajouté une valeur par défaut, comme demandé dans une des variations possibles.
 */
 const populateMasterSelector = () => {
-    /* Au dessus de ton code */
+    let defaultOption = document.createElement("option");
+    defaultOption.text = "-- Please choose a master --";
+    masterSelector.add(defaultOption, null)
 
-    /* En dessous de ton code */
+    doggletData.forEach(row => addOptionTag(row.firstName));
+}
+
+/*
+    Tu vas utiliser cette fonction dans ta boucle pour afficher une propriété dans le tableau !
+    2 arguments, ne renvoit rien. Facile.
+*/
+const addTableRow = (left, right) => masterTableBody.innerHTML += "<tr><td>" + left + "</td><td>" + right + "</td></tr>"
+
+/* Fonction pour vider le tableau. Pas d'argument, ne renvoit rien. */
+const resetTable = () => masterTableBody.innerHTML = ""
+
+/*
+    Tu vas écrire dans cette fonction !
+    1. De quoi vider le tableau,
+    2. Un filter() pour sélectionner le maître adéquat,
+    3. Une boucle pour passer à travers les propriétés de ce maître,
+    4. Une condition pour ne pas afficher une propriété qui serait un array (en l'occurrence les chiens).
+*/
+const fillMasterTable = (selectedMasterFirstName) => {
+    resetTable()
+
+    let filteredMaster = doggletData.filter(master => master.firstName === selectedMasterFirstName)[0]
+    
+    for (const property in filteredMaster) {
+        if (typeof filteredMaster[property] !== 'object') {
+            addTableRow(property, filteredMaster[property])
+        }
+    }
 }
 
 /* 
-    ci-dessous, c'est le bout de code qui dit qu'au chargement de la page, on doit executer la fonction ci-dessus, populateMasterSelector.
+    ci-dessous, c'est le bout de code qui dit qu'au chargement de la page, on doit exécuter la fonction ci-dessus, populateMasterSelector.
     l'objet 'window' représente l'onglet du navigateur (ça fait partie de l'API des navigateurs internet, tout comme 'document' que tu retrouves plus haut). 
     Sa méthode addEventListener permet de déclencher une fonction (2ème argument) lorsqu'un évènement particulier se produit, ici la fin du chargement de la page.
  */
 window.addEventListener('load', populateMasterSelector())
+masterSelector.addEventListener('change', (e) => fillMasterTable(e.target.value))
